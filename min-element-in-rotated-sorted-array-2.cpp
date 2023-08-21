46f1cdb918429b9674ed5fdf83f06f73f142340d
RhoNit <tb.edumax29@gmail.com>
2023-08-22 00:10:04 +0530

    [LC 154] Minimum Element in Rotated Sorted Arr (with duplicates)

--- min-element-in-rotated-sorted-array-2.cpp
+++ min-element-in-rotated-sorted-array-2.cpp
@@ -1,4 +1,37 @@
 class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int start = 0, end = nums.size()-1;
+        int ans = INT_MAX;
+        
+        while(start <= end) {
+            int mid = start + (end-start)/2;
+
+            if(nums[start] == nums[mid] and nums[mid] == nums[end]) {
+                ans = min(ans, nums[end]);
+                start += 1;
+                end -= 1;
+                continue;
+            }
+
+            // if left part of the mid is sorted, take the smallest element and compare it with the right part i.e. sorted part
+            if(nums[start] <= nums[mid]) {
+                ans = min(ans, nums[start]);
+                start = mid+1;
+            }
+
+            // if right part of the mid is sorted, take the smallest element and compare it with the left part i.e. sorted part
+            else {
+                ans = min(ans, nums[mid]);
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+//2nd approach
+class Solution {
 public:   
     int findMin(vector<int>& nums) {
         int n = nums.size();
